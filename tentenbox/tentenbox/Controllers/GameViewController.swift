bc58e6aea52c357db897d2c589caa25e8c750a01
mkuzmenko <m.kuzmenko@mobilunity.com>
2018-03-02 17:56:09 +0200

    prepared project for development

---
+++ tentenbox/tentenbox/Controllers/GameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameViewController.swift
+//  tentenbox
+//
+//  Created by Mikhail Kuzmenko on 3/2/18.
+//  Copyright © 2018 Mikhail Kuzmenko. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
